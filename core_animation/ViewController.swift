4fbb705cf918293ece238d5bcc95ee38b1e22e6f
Jeffery Huang <jeffery@mymac.local>
2017-12-18 21:52:23 -0800

    Simulate ImageView zooming

--- core_animation/ViewController.swift
+++ core_animation/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let startingFrame = CGRect(x: 0, y: 0, width: 200, height: 100)
 
+    lazy var zoomImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.frame = self.startingFrame
+        imageView.backgroundColor = .red
+        imageView.image = UIImage(named: "zuckdog")
+        imageView.contentMode = .scaleAspectFill
+        
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animate)))
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addSubview(zoomImageView)
     }
+    
+    @objc func animate() {
+        UIView.animate(withDuration: 0.75) {
+            let height = (self.view.frame.width / self.startingFrame.width) * self.startingFrame.height
 
+            let y = self.view.frame.height / 2 - height / 2
+            
+            self.zoomImageView.frame = CGRect(x: 0, y: y, width: self.view.frame.width, height: height)
+        }
+    }
 
 }
 
